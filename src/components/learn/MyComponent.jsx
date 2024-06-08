a1eab4ae1be35a1b778873fc221b9b688218854f
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-08 21:25:05 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,16 @@
+//JSX : 1 parent
+//fragment
+
+import './style.css';
 
 const MyComponent = () => {
     return (
-        <div> eric & hoidanit update</div> //tag
+        <>
+            <div> eric & hoidanit update</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
     );
 }
 
