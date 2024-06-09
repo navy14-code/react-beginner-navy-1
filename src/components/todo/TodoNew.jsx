81d31d8c42c6a6a15696301bbcfab7c267d6d39d
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-09 16:18:04 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,11 +3,23 @@ const TodoNew = (props) => {
     console.log(">>> check point: ", props)
     const { addNewTodo } = props;
 
-    // addNewTodo("eric")
+    // addNewTodo("eric") //fire
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name)
+    }
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
