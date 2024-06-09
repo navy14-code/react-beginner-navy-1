6bee052daf781cb3228df5450418ab6c1010916e
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-09 15:35:33 +0700

    video 35

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,5 +1,9 @@
 
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log(">>> check point: ", props)
+    const { addNewTodo } = props;
+
+    // addNewTodo("eric")
     return (
         <div className='todo-new'>
             <input type="text" />
