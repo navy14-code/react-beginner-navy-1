b2d89543e1e05550c4008bd98e6d2f603369a902
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-11 12:55:15 +0700

    video 48

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import reactLogo from './assets/react.svg';
 import { useState } from 'react';
 import Header from './components/layout/header';
 import Footer from './components/layout/footer';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
 
@@ -34,6 +35,7 @@ const App = () => {
   //{key:value}
   return (
     <>
+
       <Header />
       <div className="todo-container">
         <div className="todo-title">Todo List</div>
@@ -65,6 +67,7 @@ const App = () => {
       } */}
 
       </div>
+      <Outlet />
       <Footer />
     </>
   )
