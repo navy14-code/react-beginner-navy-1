96ebbe18993965a3a45f030aa19e62d7512aca70
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-15 13:43:43 +0700

    video 78

--- src/components/user/update.user.modal.jsx
+++ src/components/user/update.user.modal.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import { Input, notification, Modal } from "antd";
-import { createUserAPI } from "../../services/api.service";
+import { createUserAPI, updateUserAPI } from "../../services/api.service";
 
 const UpdateUserModal = (props) => {
     const [id, setId] = useState("");
     const [fullName, setFullName] = useState("");
     const [phone, setPhone] = useState("");
 
-    const { isModalUpdateOpen, setIsModalUpdateOpen, dataUpdate, setDataUpdate } = props;
+    const { isModalUpdateOpen, setIsModalUpdateOpen,
+        dataUpdate, setDataUpdate,
+        loadUser
+    } = props;
 
     //next dataUpdate != prev dataUpdate
     useEffect(() => {
-        console.log(">>> check dataUpdate props: ", dataUpdate)
         if (dataUpdate) {
             setId(dataUpdate._id)
             setFullName(dataUpdate.fullName);
@@ -20,14 +22,14 @@ const UpdateUserModal = (props) => {
     }, [dataUpdate])
 
     const handleSubmitBtn = async () => {
-        const res = await createUserAPI(fullName, email, password, phone);
+        const res = await updateUserAPI(id, fullName, phone);
         if (res.data) {
             notification.success({
-                message: "Create user",
-                description: "Tạo user thành công"
+                message: "Update user",
+                description: "Cập nhât thành công"
             })
             resetAndCloseModal();
-            // await loadUser();
+            await loadUser();
 
         } else {
             notification.error({
