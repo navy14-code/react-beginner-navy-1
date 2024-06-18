b64dc8e134cce5516e2641214a308bdb3374c810
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-18 17:13:06 +0700

    video 99

--- src/components/context/auth.context.jsx
+++ src/components/context/auth.context.jsx
@@ -22,6 +22,7 @@ export const AuthWrapper = (props) => {
     return (
         <AuthContext.Provider value={{ user, setUser }}>
             {props.children}
+            {/* <RouterProvider router={router} /> */}
         </AuthContext.Provider>
     )
 }
--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,6 +1,9 @@
 import { Link, NavLink } from 'react-router-dom';
 import { Menu } from 'antd';
-import { UsergroupAddOutlined, HomeOutlined, AuditOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+    UsergroupAddOutlined, LoginOutlined,
+    HomeOutlined, AuditOutlined, AliwangwangOutlined
+} from '@ant-design/icons';
 import { useContext, useState } from 'react';
 import { AuthContext } from '../context/auth.context';
 
@@ -33,21 +36,24 @@ const Header = () => {
             icon: <AuditOutlined />,
         },
 
-        {
-            label: 'Cài đặt',
+        ...(!user.id ? [{
+            label: <Link to={"/login"}>Đăng nhập</Link>,
+            key: 'login',
+            icon: <LoginOutlined />,
+        }] : []),
+
+        ...(user.id ? [{
+            label: `Welcome ${user.fullName}`,
             key: 'setting',
-            icon: <SettingOutlined />,
+            icon: <AliwangwangOutlined />,
             children: [
-                {
-                    label: <Link to={"/login"}>Đăng nhập</Link>,
-                    key: 'login',
-                },
                 {
                     label: 'Đăng xuất',
                     key: 'logout',
                 },
             ],
-        },
+        }] : []),
+
 
     ];
 
