01631e3eae08e0d20fd698e290796c5fb87fb742
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-21 12:45:23 +0700

    video 110

--- src/components/book/book.table.jsx
+++ src/components/book/book.table.jsx
@@ -6,6 +6,7 @@ import BookDetail from "./book.detail";
 import CreateBookControl from "./create.book.control";
 import CreateBookUncontrol from "./create.book.uncontrol";
 import UpdateBookControl from "./update.book.control";
+import UpdateBookUncontrol from "./update.book.uncontrol";
 
 const BookTable = () => {
 
@@ -180,13 +181,22 @@ const BookTable = () => {
                 loadBook={loadBook}
             />
 
-            <UpdateBookControl
+            {/* <UpdateBookControl
+                dataUpdate={dataUpdate}
+                setDataUpdate={setDataUpdate}
+                isModalUpdateOpen={isModalUpdateOpen}
+                setIsModalUpdateOpen={setIsModalUpdateOpen}
+                loadBook={loadBook}
+            /> */}
+
+            <UpdateBookUncontrol
                 dataUpdate={dataUpdate}
                 setDataUpdate={setDataUpdate}
                 isModalUpdateOpen={isModalUpdateOpen}
                 setIsModalUpdateOpen={setIsModalUpdateOpen}
                 loadBook={loadBook}
             />
+
         </>
     )
 }
