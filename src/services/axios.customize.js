8123811bab981fb03fc79e775bb05a96ce57bd6b
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-06-21 13:06:13 +0700

    video 112

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,4 +1,11 @@
 import axios from "axios";
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 100,
+});
+
 
 // Set config defaults when creating the instance
 const instance = axios.create({
@@ -9,6 +16,7 @@ const instance = axios.create({
 //   instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
 instance.interceptors.request.use(function (config) {
+    NProgress.start();
     if (typeof window !== "undefined" && window
         && window.localStorage
         && window.localStorage.getItem('access_token')) {
@@ -17,6 +25,8 @@ instance.interceptors.request.use(function (config) {
     // Do something before request is sent
     return config;
 }, function (error) {
+    NProgress.done();
+
     // Do something with request error
     return Promise.reject(error);
 });
@@ -24,11 +34,15 @@ instance.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+    NProgress.done();
+
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     if (response.data && response.data.data) return response.data;
     return response;
 }, function (error) {
+    NProgress.done();
+
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
 
