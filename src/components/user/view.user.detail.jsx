fb5a9104cb9bd0b6b5cdef8e033ec29ba7ac797e
navy14-code <vtnam12k24@gmail.com>
2024-11-22 00:06:45 +0700

    create buttun upload

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from "antd"
+import { Button, Drawer, Upload } from "antd"
 
 const ViewUserDetail = (props) => {
 
@@ -10,7 +10,9 @@ const ViewUserDetail = (props) => {
     } = props;
 
     return (
-        <Drawer title="Detail Users"
+        <Drawer
+            width={`40vw`}
+            title="Detail Users"
             onClose={() => {
                 setDataDetail(null);
                 setIsDetailOpen(false);
@@ -19,14 +21,42 @@ const ViewUserDetail = (props) => {
         >
             {dataDetail ?
                 <>
-                    <p>Id : {dataDetail._id}</p>
-                    <br />
-                    <p>Full name : {dataDetail.fullName}</p>
-                    <br />
-                    <p>Email : {dataDetail.email}</p>
-                    <br />
-                    <p>Phone : {dataDetail.phone}</p>
-                    <br />
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <div style={{ flex: '1', textAlign: 'center' }}>
+
+                            <br />
+                            <img
+                                height={250}
+                                width={250}
+                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                            <div style={{ textAlign: 'center', padding: '20px' }}>
+                                <label htmlFor="btnUpload" style={{
+                                    cursor: 'pointer',
+                                    background: 'gold',
+                                    display: 'block',
+                                    marginTop: '15px',
+                                    padding: '5px 10px',
+                                    width: "fit-content",
+                                    borderRadius: '5px'
+                                }}>
+                                    Upload Avatar
+                                </label>
+                                <input type="file" hidden id="btnUpload" name="myfile"></input>
+                            </div>
+                            {/* <Button type="primary" style={{ textAlign: 'left', marginTop: '10px' }}>Upload Avatar</Button> */}
+                        </div>
+                        <div style={{ flex: '2', paddingLeft: '50px' }}>
+                            <p>Id : {dataDetail._id}</p>
+                            <br />
+                            <p>Full name : {dataDetail.fullName}</p>
+                            <br />
+                            <p>Email : {dataDetail.email}</p>
+                            <br />
+                            <p>Phone : {dataDetail.phone}</p>
+                            <br />
+                        </div>
+                    </div>
+
                 </>
                 :
                 <>
