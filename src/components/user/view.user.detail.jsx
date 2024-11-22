de88773e824ad36273a21e5df95762bd3050d458
navy14-code <vtnam12k24@gmail.com>
2024-11-22 16:24:45 +0700

    preview

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,5 @@
 import { Button, Drawer, Upload } from "antd"
+import { useState } from "react";
 
 const ViewUserDetail = (props) => {
 
@@ -8,6 +9,23 @@ const ViewUserDetail = (props) => {
         isDetailOpen,
         setIsDetailOpen
     } = props;
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+
+    const handleOnChange = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null);
+            setPreview(null);
+            return;
+        }
+        const file = event.target.files[0]
+        if (file) {
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file));
+        }
+        console.log('check file', preview)
+    }
 
     return (
         <Drawer
@@ -22,28 +40,17 @@ const ViewUserDetail = (props) => {
             {dataDetail ?
                 <>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <div style={{ flex: '1', textAlign: 'center' }}>
-
-                            <br />
+                        <div
+                            style={{
+                                flex: '1',
+                                textAlign: 'center',
+                                marginTop: '10px',
+                                height: '100%', width: '100%',
+                                border: '2px solid #ccc'
+                            }}>
                             <img
-                                height={250}
-                                width={250}
+                                style={{ height: '100%', width: '100%', objectFit: 'contain' }}
                                 src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
-                            <div style={{ textAlign: 'center', padding: '20px' }}>
-                                <label htmlFor="btnUpload" style={{
-                                    cursor: 'pointer',
-                                    background: 'gold',
-                                    display: 'block',
-                                    marginTop: '15px',
-                                    padding: '5px 10px',
-                                    width: "fit-content",
-                                    borderRadius: '5px'
-                                }}>
-                                    Upload Avatar
-                                </label>
-                                <input type="file" hidden id="btnUpload" name="myfile"></input>
-                            </div>
-                            {/* <Button type="primary" style={{ textAlign: 'left', marginTop: '10px' }}>Upload Avatar</Button> */}
                         </div>
                         <div style={{ flex: '2', paddingLeft: '50px' }}>
                             <p>Id : {dataDetail._id}</p>
@@ -55,6 +62,41 @@ const ViewUserDetail = (props) => {
                             <p>Phone : {dataDetail.phone}</p>
                             <br />
                         </div>
+                    </div>
+                    <div style={{ textAlign: 'center', padding: '20px' }}>
+                        <label htmlFor="btnUpload" style={{
+                            cursor: 'pointer',
+                            background: 'gold',
+                            display: 'block',
+                            marginTop: '15px',
+                            padding: '5px 10px',
+                            width: "fit-content",
+                            borderRadius: '5px'
+                        }}>
+                            Upload Avatar
+                        </label>
+                        <input
+                            onChange={(event) => { handleOnChange(event) }}
+                            type="file" hidden id="btnUpload" name="myfile"></input>
+                        {/* <Button
+                            type="primary"
+                            style={{ textAlign: 'left', marginTop: '10px' }}>
+                            Upload Avatar
+                        </Button> */}
+                        {preview &&
+                            <div
+                                style={{
+                                    flex: '1',
+                                    textAlign: 'center',
+                                    marginTop: '10px',
+                                    height: '30%', width: '30%',
+                                    border: '2px solid #ccc'
+                                }}>
+                                <img
+                                    style={{ height: '100%', width: '100%', objectFit: 'contain' }}
+                                    src={preview} />
+                            </div>
+                        }
                     </div>
 
                 </>
